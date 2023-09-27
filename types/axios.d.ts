744fbe71a9d255fce3d66836954edb47a4633635
wangbaohui <13802885224@139.com>
2023-09-27 10:59:54 +0800

    fix: 增加注释

--- types/axios.d.ts
+++ types/axios.d.ts
@@ -1,3 +1,14 @@
+/**
+ * 什么时候用type 什么时候用interface
+ * 一、用interface
+ * 1、定义对象的形状：interface 最常用于定义对象的形状，它们描述了对象应该具有的属性和方法
+ * 2、类的实现：interface 可以用于定义类需要实现的方法和属性。这有助于确保类符合特定的接口契约
+ * 3、继承和合并：interface 支持继承和合并，您可以将多个接口组合成一个。这对于定义复杂的接口非常有用。
+ * 二、使用type的情况
+ * 1、合类型和交叉类型：type 可以用于定义联合类型（Union Types）和交叉类型（Intersection Types）。这些类型对于描述复杂的数据结构非常有用。
+ * 2、创建类型别名：type 用于创建自定义类型别名，这有助于提高代码的可读性和可维护性
+ * 3、复杂类型操作：如果您需要进行复杂的类型操作，例如条件类型（conditional types），type 提供了更大的灵活性。
+ */
 export type ErrorMessageMode = 'none' | 'modal' | 'message' | undefined;
 export type SuccessMessageMode = ErrorMessageMode;
 
